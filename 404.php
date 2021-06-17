0f65ea6363a395341e7b44ccf3bd2e41cfc2727c
VodOSS <75663751+VodOSS@users.noreply.github.com>
2021-06-17 13:12:15 +0300

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>404 not found!</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
+</head>
+<body>
+<style>
+h1 {
+font-family: 'Nunito', sans-serif;
+text-align: center;
+}
+button {
+
+}
+</style>
+<h1>404 not found</h1>
+<button>home</button>
+</body>
+</html>
